0b760a00d2a65e649452cd2c8b3b7f9df765e5b5
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-04-19 19:27:07 -0500

    Proyecto Base

---
+++ src/main/java/com/example/televet/TelevetApplication.java
@@ -0,0 +1,13 @@
+package com.example.televet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelevetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelevetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/televet/TelevetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.televet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelevetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
