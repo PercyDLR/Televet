f019937c3c130e248a57b0a9387b016a0b167f21
Angel <a20190192@pucp.edu.pe>
2022-04-19 20:20:51 -0500

    fragmentos, index

--- src/main/java/com/example/televet/Controller/HomeController.java
+++ src/main/java/com/example/televet/Controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.example.televet.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping(value={"/", ""})
+    public String index() {
+        return "index";
+    }
 }
