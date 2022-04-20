f754a83c8eb0d1a9b8f753079b6956fd13f82e76
Elianne <a20185361@pucp.edu.pe>
2022-04-20 12:56:07 -0500

    :'v

--- src/main/java/com/example/televet/Controller/MascotaController.java
+++ src/main/java/com/example/televet/Controller/MascotaController.java
@@ -67,7 +67,6 @@ public class MascotaController {
         if(optionalMascota.isPresent()) {
             model.addAttribute("mascota", optionalMascota.get());
             List<Servicio> servicioList = servicioRepository.listaMascota(id);
-            System.out.println(servicioList.size());
             model.addAttribute("listaServicios", servicioList);
             model.addAttribute("edad", servicioRepository.edad(id));
             return "mascotas/details";
